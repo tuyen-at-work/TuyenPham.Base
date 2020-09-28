ff1ad7eda9f969f2df0188edcb3fe923aa861a00
Tuyen Pham <tuyen1106@live.com>
2020-09-28 21:33:01 +0700

    [*] Update WebConfigHelper

--- src/TuyenPham.Base/Helpers/WebConfigHelper.cs
+++ src/TuyenPham.Base/Helpers/WebConfigHelper.cs
@@ -1,6 +1,43 @@
-﻿namespace TuyenPham.Base.Helpers
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace TuyenPham.Base.Helpers
 {
     public static partial class WebConfigHelper
     {
+        public static XDocument SetConnectionString(
+            this XDocument xDoc,
+            string name,
+            string connectionString,
+            params KeyValuePair<string, string>[] attributes)
+        {
+            var connectionStrings = xDoc.XPathSelectElement("/configuration/connectionStrings");
+
+            var conn = connectionStrings
+                .GetOrCreateElement(
+                    "add",
+                    new KeyValuePair<string, string>("name", name));
+
+            conn.SetAttribute("connectionString", connectionString);
+
+            foreach (var (k, v) in attributes)
+                conn.SetAttribute(k, v);
+
+            return xDoc;
+        }
+
+        public static XDocument SetSqlConnectionString(
+            this XDocument xDoc,
+            string name,
+            string connectionString)
+        {
+            xDoc.SetConnectionString(
+                name,
+                connectionString,
+                new KeyValuePair<string, string>("providerName", "System.Data.SqlClient"));
+
+            return xDoc;
+        }
     }
 }
