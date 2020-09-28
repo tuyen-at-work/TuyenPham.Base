209960370733d45c77dfe74bfa0b3e87428676c3
tuyen-at-work <68000455+tuyen-at-work@users.noreply.github.com>
2020-09-28 14:56:36 +0700

    [+] Add TuyenPham.Episerver

---
+++ src/TuyenPham.Episerver/ConfigHelper.Blob.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using TuyenPham.Base.Helpers;
+
+namespace TuyenPham.Episerver
+{
+    public static partial class ConfigHelper
+    {
+        public static XElement GetBlobProvidersElement(this XDocument xDoc)
+        {
+            return xDoc
+                .XPathSelectElement("/configuration/episerver.framework/blob");
+        }
+
+        public static XElement SetDefaultProvider(this XElement element, string providerName)
+        {
+            element.SetAttribute("defaultProvider", providerName);
+
+            return element;
+        }
+
+        public static XElement AddOrUpdateAzureBlob(
+            this XElement element,
+            string name,
+            string container)
+        {
+            var child = element
+                .GetOrCreateElement("providers")
+                .GetOrCreateElement("add", new KeyValuePair<string, string>("name", name));
+
+            child
+                .SetAttribute("name", name)
+                .SetAttribute("type", "EPiServer.Azure.Blobs.AzureBlobProvider,EPiServer.Azure")
+                .SetAttribute("connectionStringName", "EPiServerAzureBlobs")
+                .SetAttribute("container", container);
+
+            return element;
+        }
+
+        public static XElement AddFileBlob(
+            this XElement element,
+            string name,
+            string path)
+        {
+            var child = element
+                .GetOrCreateElement("providers")
+                .GetOrCreateElement("add", new KeyValuePair<string, string>("name", name));
+
+            child
+                .SetAttribute("type", "EPiServer.Framework.Blobs.FileBlobProvider, EPiServer.Framework")
+                .SetAttribute("path", path);
+
+            return element;
+        }
+    }
+}
---
+++ src/TuyenPham.Episerver/ConfigHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Linq;
+using System.Xml.XPath;
+using TuyenPham.Base.Helpers;
+
+namespace TuyenPham.Episerver
+{
+    public static partial class ConfigHelper
+    {
+        public static XElement SetEpiserverFind(
+            this XDocument xDoc,
+            string serviceUrl,
+            string defaultIndex)
+        {
+            var episerverFind = xDoc.XPathSelectElement("/configuration/episerver.find")
+                ?.SetAttribute("serviceUrl", serviceUrl)
+                ?.SetAttribute("defaultIndex", defaultIndex);
+
+            return episerverFind;
+        }
+    }
+}
